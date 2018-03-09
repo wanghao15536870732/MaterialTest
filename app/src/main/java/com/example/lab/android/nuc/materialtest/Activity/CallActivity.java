c590cd5f20622de8115c9ffef0d1ecb03a6d4157
wanghao <1484290617@qq.com>
2018-03-09 18:11:50 +0800

    电话簿的UI优化.

--- app/src/main/java/com/example/lab/android/nuc/materialtest/Activity/CallActivity.java
+++ app/src/main/java/com/example/lab/android/nuc/materialtest/Activity/CallActivity.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -26,15 +27,18 @@ import com.example.lab.android.nuc.materialtest.Call.ContactsAdapter;
 import com.example.lab.android.nuc.materialtest.R;
 
 import java.lang.ref.SoftReference;
+import java.sql.BatchUpdateException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CallActivity extends AppCompatActivity {
+public class CallActivity extends AppCompatActivity implements View.OnClickListener{
 
     private List<Contacts> contactList = new ArrayList<>();
 
     ContactsAdapter adapter;
 
+    private Button backButton_2;
+    private Button callButton;
 
 
     @Override
@@ -42,7 +46,11 @@ public class CallActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
 
+        backButton_2 = (Button) findViewById(R.id.title_back_2);
+        callButton = (Button) findViewById(R.id.title_call);
 
+        backButton_2.setOnClickListener(this);
+        callButton.setOnClickListener(this);
         //建立ListView的实例
         ListView listView = (ListView) findViewById(R.id.contacts_view);
         adapter = new ContactsAdapter(CallActivity.this,
@@ -80,7 +88,7 @@ public class CallActivity extends AppCompatActivity {
             if (cursor != null){
                 while (cursor.moveToNext()){
                     //获取联系人姓名
-                    Contacts contacts = new Contacts(R.drawable.mango_pic);
+                    Contacts contacts = new Contacts(R.drawable.strawberry_pic);
                     String displayName = cursor.getString(cursor.getColumnIndex
                             (ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                     String number = cursor.getString(cursor.getColumnIndex
@@ -115,77 +123,15 @@ public class CallActivity extends AppCompatActivity {
         }
     }
 
-
-//    ArrayAdapter<String> adapter;
-//
-//    List<String> contactsList = new ArrayList<>();
-//
-//    Contacts contacts = new Contacts();
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_call);
-//
-//        //设置ListView的点击事件
-//        ListView contactsView = (ListView) findViewById(R.id.contacts_view);
-//
-//
-//        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contactsList);
-//        //将adpter设置进去
-//        contactsView.setAdapter(adapter);
-//
-//        //申请电话本的权限
-//        if (ContextCompat.checkSelfPermission(CallActivity.this, Manifest.permission.
-//                READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
-//            ActivityCompat.requestPermissions(CallActivity.this,new String[]{Manifest.
-//                    permission.READ_CONTACTS},1);
-//        }else{
-//            readContacts();
-//        }
-//    }
-//
-//
-//    //查询联系人数据
-//    private void readContacts(){
-//        Cursor cursor = null;
-//        try{
-//            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-//                    null,null,null,null);
-//            if (cursor != null){
-//                while (cursor.moveToNext()){
-//                    //获取联系人姓名
-//                    String displayName = cursor.getString(cursor.getColumnIndex
-//                            (ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-//                    String number = cursor.getString(cursor.getColumnIndex
-//                            (ContactsContract.CommonDataKinds.Phone.NUMBER));
-//                    contacts.setContactName(displayName);
-//                    contacts.setContactNumber(number);
-//                    contactsList.add(displayName + "\n" + number);
-//                }
-//                adapter.notifyDataSetChanged();
-//            }
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }finally {
-//            //如果cursor存在
-//            if (cursor != null){
-//                cursor.close();
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        switch (requestCode){
-//            case 1:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                    readContacts();
-//                }else {
-//                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
-//                }
-//                break;
-//            default:
-//        }
-//    }
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_back_2:
+                finish();
+                break;
+            case R.id.title_call:
+                Intent call_intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(call_intent);
+        }
+    }
 }
